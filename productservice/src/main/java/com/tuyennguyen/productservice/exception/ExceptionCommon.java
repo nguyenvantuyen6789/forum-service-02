43aae299237a5308640d1a5e51c96d3a2cb286da
Admin <nguyenvantuyen6789@gmail.com>
2022-03-26 18:16:06 +0700

    Create Exception Common

--- productservice/src/main/java/com/tuyennguyen/productservice/exception/ExceptionCommon.java
+++ productservice/src/main/java/com/tuyennguyen/productservice/exception/ExceptionCommon.java
@@ -2,8 +2,15 @@ package com.tuyennguyen.productservice.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,9 +19,34 @@ public class ExceptionCommon {
 
     Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    public Map<String, String> conflictData() {
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public Map<String, String> conflict(Exception e) {
         Map<String, String> map = new HashMap<>();
-        return null;
+        map.put("code", "409");
+        map.put("error", "Conflict Data");
+
+        return map;
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    public Map<String, String> methodNotSupportException(Exception e) {
+        Map<String, String> map = new HashMap<>();
+        map.put("code", "405");
+        map.put("error", "Method Not Allow");
+
+        return map;
+    }
+
+    @ExceptionHandler({HttpMessageNotReadableException.class, MethodArgumentNotValidException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> badRequest(Exception e) {
+        Map<String, String> map = new HashMap<>();
+        map.put("code", "400");
+        map.put("error", "Bad Request");
+
+        return map;
     }
 
 }
